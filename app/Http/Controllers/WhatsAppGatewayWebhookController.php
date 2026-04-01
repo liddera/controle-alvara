b5867b38cbd5584296fdc79022d446309d25999d
liddera <dev@lidderacont.com.br>
2026-04-01 08:41:38 -0400

    chekin

--- app/Http/Controllers/WhatsAppGatewayWebhookController.php
+++ app/Http/Controllers/WhatsAppGatewayWebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\WhatsApp\WhatsAppWebhookService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WhatsAppGatewayWebhookController extends Controller
 {
@@ -12,6 +13,12 @@ class WhatsAppGatewayWebhookController extends Controller
 
     public function __invoke(Request $request, ?string $event = null): JsonResponse
     {
+        Log::info('Webhook WhatsApp payload bruto.', [
+            'event' => $event,
+            'body' => $request->getContent(),
+            'json' => $request->all(),
+        ]);
+
         $this->service->handle($event, $request);
 
         return response()->json(['ok' => true]);
